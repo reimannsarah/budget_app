60b8ac84e7329f460ebab8668c5bec8fd1081f8e
Sarah Reimann <reimannsarah45@gmail.com>
2025-02-09 13:44:33 -0500

    add views

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import BudgetItem, CreditCard, Income, Loan, PaymentMethod, Subscription
+from .serializers import (
+    BudgetItemSerializer,
+    CreditCardSerializer,
+    IncomeSerializer,
+    LoanSerializer,
+    PaymentMethodSerializer,
+    SubscriptionSerializer,
+)
 
-# Create your views here.
+# BudgetItem Views
+class BudgetItemListCreateView(generics.ListCreateAPIView):
+    queryset = BudgetItem.objects.all()
+    serializer_class = BudgetItemSerializer
+
+class BudgetItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BudgetItem.objects.all()
+    serializer_class = BudgetItemSerializer
+
+# CreditCard Views
+class CreditCardListCreateView(generics.ListCreateAPIView):
+    queryset = CreditCard.objects.all()
+    serializer_class = CreditCardSerializer
+
+class CreditCardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CreditCard.objects.all()
+    serializer_class = CreditCardSerializer
+
+# Income Views
+class IncomeListCreateView(generics.ListCreateAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+class IncomeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+# Loan Views
+class LoanListCreateView(generics.ListCreateAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+# PaymentMethod Views
+class PaymentMethodListCreateView(generics.ListCreateAPIView):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
+
+class PaymentMethodDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
+
+# Subscription Views
+class SubscriptionListCreateView(generics.ListCreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+class SubscriptionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
