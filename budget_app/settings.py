a93f410b1e9f05df7bcad3e331cf6b86a012f067
Sarah Reimann <reimannsarah45@gmail.com>
2025-02-09 13:44:19 -0500

    register apps

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+# My apps
     'finance',
     'users'
+
+# Third-party frameworks
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
